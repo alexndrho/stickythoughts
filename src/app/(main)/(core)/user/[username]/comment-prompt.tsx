0ac520e114eb879f846d3ce8affb295848636e50
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2026-02-03 21:27:15 +0800

    fix: remove nested title

--- src/app/(main)/(core)/user/[username]/comment-prompt.tsx
+++ src/app/(main)/(core)/user/[username]/comment-prompt.tsx
@@ -8,31 +8,17 @@ export interface CommentPromptProps {
 }
 
 export default function CommentPrompt({ isOwnProfile }: CommentPromptProps) {
+  const title = isOwnProfile
+    ? "You haven't created any comments yet."
+    : "This user hasn't created any comments yet.";
+
   return (
     <div className={classes["tab-prompt"]}>
       <IconNote size="3rem" className={classes["tab-prompt__icon"]} />
 
       <Title order={2} size="h3" className={classes["tab-prompt__title"]}>
-        {isOwnProfile ? <OwnProfileContent /> : <OtherProfileContent />}
+        {title}
       </Title>
     </div>
-  );
-}
-
-function OwnProfileContent() {
-  return (
-    <>
-      <Title order={2} size="h3" className={classes["tab-prompt__title"]}>
-        You haven&apos;t created any comments yet.
-      </Title>
-    </>
-  );
-}
-
-function OtherProfileContent() {
-  return (
-    <Title order={2} size="h3" className={classes["tab-prompt__title"]}>
-      This user hasn&apos;t created any comments yet.
-    </Title>
   );
 }
