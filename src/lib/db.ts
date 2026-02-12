df41a0f1c90fd7764205a768f2aeeb73ccdcb889
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2026-02-12 13:37:19 +0800

    fix(db): extend Prisma client with accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,7 @@ import "server-only";
 
 import "dotenv/config";
 import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const databaseUrl = process.env.DATABASE_URL!;
@@ -11,13 +12,13 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const prismaClient = databaseUrl.startsWith("prisma://")
   ? new PrismaClient({
       accelerateUrl: databaseUrl,
-    })
+    }).$extends(withAccelerate())
   : new PrismaClient({
       adapter: new PrismaPg({
         connectionString: databaseUrl,
       }),
     });
 
-export const prisma = globalForPrisma.prisma || prismaClient;
+export const prisma = globalForPrisma.prisma ?? prismaClient;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
